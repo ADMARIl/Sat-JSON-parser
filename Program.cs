124e47322c1ff4c6c744399f0c597a02e0a0fc26
Chase Carbaugh <chasec1@umbc.edu>
2019-11-25 17:43:17 -0500

    Rough implementation of CSV parser

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Microsoft.VisualBasic.FileIO;
 using static CelestialBody;
 
 namespace Sat_JSON_parser
@@ -14,30 +15,50 @@ namespace Sat_JSON_parser
             ConvertFromJSON("CleanedData.json");
         }
 
-        static CelestialBody[] ConvertFromJSON(string filename) {
+        static CelestialBody[] ConvertFromJSON(string filename)
+        {
             Console.WriteLine("Bottom Text got that Bottom Text");
 
+
+            var path = filename;
+            using (TextFieldParser csvParser = new TextFieldParser(path))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                csvParser.ReadLine();
+
+                while (!csvParser.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    string[] fields = csvParser.ReadFields();
+                    string Name = fields[0];
+                    string Address = fields[1];
+                }
+            }
             // HELLO CHAS
             // Hello Andre
-           // Write something here to maybe convert the CleanedData.json file and
-           //   export like an array/vector of satellite objects (class CelestialBody???)
+            // Write something here to maybe convert the CleanedData.json file and
+            //   export like an array/vector of satellite objects (class CelestialBody???)
 
-           /* 
-               TODO: Brendan requests "Can you guys write a Json parser for our data and make an array of
-               to roll with in the Unity envt?"
-           */
-           // I suspect the units for the Cleaned data is metric but I have no idea which one
-           // read in file to one big string
-           //string jsonString = ;
+            /* 
+                TODO: Brendan requests "Can you guys write a Json parser for our data and make an array of
+                to roll with in the Unity envt?"
+            */
+            // I suspect the units for the Cleaned data is metric but I have no idea which one
+            // read in file to one big string
+            //string jsonString = ;
 
-           //var satIDs = JsonConvert.DeserializeObject<CelestialBody>(File.ReadAllText(filename));
+            //var satIDs = JsonConvert.DeserializeObject<CelestialBody>(File.ReadAllText(filename));
 
-           CelestialBody tempCB = new CelestialBody();
+            CelestialBody tempCB = new CelestialBody();
 
             CelestialBody[] bodies = new CelestialBody[500];
             //bodies[0] = ;
             return bodies;
-           //GameObject temp = new GameObject();
+            //GameObject temp = new GameObject();
 
         }
     }
