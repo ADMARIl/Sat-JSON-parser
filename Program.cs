aed0718d284ceecbf3e82f7923936f09f5c3c7f7
Andrew Ingson <aings1@umbc.edu>
2019-11-25 16:48:32 -0500

    hello chas

--- Program.cs
+++ Program.cs
@@ -16,6 +16,9 @@ namespace Sat_JSON_parser
 
         static CelestialBody[] ConvertFromJSON(string filename) {
             Console.WriteLine("Bottom Text got that Bottom Text");
+
+            // HELLO CHAS
+
            // Write something here to maybe convert the CleanedData.json file and
            //   export like an array/vector of satellite objects (class CelestialBody???)
 
